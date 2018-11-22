5992dd6d3165e6ee5e4f7f41276106fbda06b04e
ozgurkaracam <ozgurkrcm@hotmail.com>
2018-11-22 12:08:57 +0300

    namespace broadcast

---
+++ namespace/app.js
@@ -0,0 +1,18 @@
+const http=require('http');
+const socketio=require('socket.io');
+const server=http.createServer((req,res)=>{
+
+});
+
+server.listen(3000);
+
+const io=socketio.listen(server);
+
+const nsp=io.of("/creative93");
+
+nsp.on("connection",(socket)=>{
+    console.log("connected");
+    socket.on("isimyaz",()=>{
+       socket.broadcast.emit("gonder",{isim : "özgür"});
+    });
+});
